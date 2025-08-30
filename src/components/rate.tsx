b44e9755329a14400898112a0b63894185680db9
Nikita Kononenko <jadnast@hikasami.com>
2025-08-30 04:34:27 -0700

    fix: Rate Button

--- src/components/rate.tsx
+++ src/components/rate.tsx
@@ -11,7 +11,7 @@ import { cva } from 'class-variance-authority';
 import { usePathname } from 'next/navigation';
 
 const rateButtonVariants = cva(
-  'inline-flex items-center gap-2 px-3 py-2 rounded-full font-medium border text-sm [&_svg]:size-4 disabled:cursor-not-allowed',
+  'inline-flex items-center gap-2 px-3 py-2 rounded-full font-medium border cursor-pointer text-sm [&_svg]:size-4 disabled:cursor-not-allowed',
   {
     variants: {
       active_good: {
